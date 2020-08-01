29b85e18fa94a456a17730376de458f480051e96
Jorge Moreira <jorge.smrr@gmail.com>
2020-08-01 15:38:23 -0300

    Knobs added for storybook

--- src/components/form/text-field/TextField.stories.js
+++ src/components/form/text-field/TextField.stories.js
@@ -1,18 +1,52 @@
 import TextField from "./TextField";
+import { withKnobs, text, boolean } from "@storybook/addon-knobs";
+
 export default {
   title: "TextField",
-  // Our exports that end in "Data" are not stories.
-  excludeStories: /.*Data$/,
+  decorators: [withKnobs],
 };
 
-// default task state
 export const Default = () => ({
   components: { TextField },
-  template: `<TextField label="Example" placeholder="Type something..."/>`,
+  props: {
+    label: {
+      default: text("label", "My field"),
+    },
+    placeholder: {
+      default: text("placeholder", "Type something here..."),
+    },
+  },
+  template: `<TextField :label="label" :placeholder="placeholder"/>`,
 });
 
-// default task state
 export const Invalid = () => ({
   components: { TextField },
-  template: `<TextField label="Example" error="Required field" />`,
+  props: {
+    label: {
+      default: text("label", "My field"),
+    },
+    placeholder: {
+      default: text("placeholder", "Type something here..."),
+    },
+    error: {
+      default: boolean("error", true),
+    },
+  },
+  template: `<TextField :label="label" :placeholder="placeholder" :error="error"/>`,
+});
+
+export const InvalidWithMessage = () => ({
+  components: { TextField },
+  props: {
+    label: {
+      default: text("label", "My field"),
+    },
+    placeholder: {
+      default: text("placeholder", "Type something here..."),
+    },
+    error: {
+      default: text("error", "Required field"),
+    },
+  },
+  template: `<TextField :label="label" :placeholder="placeholder" :error="error"/>`,
 });
